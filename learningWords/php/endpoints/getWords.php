0ab0eee43274d73ae67915407dcfad9d49ec012e
Bialywilku412 <maciekplewka0702@gmail.com>
2025-05-20 10:03:07 +0200

    redirection fixes

--- learningWords/php/endpoints/getWords.php
+++ learningWords/php/endpoints/getWords.php
@@ -1,11 +1,22 @@
 <?php
 
-require_once "..classes/Database.php";
-require_once "..classes/Words.php";
-
 header('Content-Type: application/json');
-$data = json_decode(file_get_contents("php://input"), true);
 
-$words = Words::getWords();
+require_once __DIR__ . '/../classes/Words.php';
 
-exit(json_encode(array("valid" => true, "words" => $words)));
+try {
+    $words = Words::getWords();
+
+    echo json_encode([
+        "valid" => true,
+        "words" => $words
+    ]);
+} catch (Exception $e) {
+    // W razie błędu zwracamy JSON z info o błędzie
+    echo json_encode([
+        "valid" => false,
+        "error" => $e->getMessage()
+    ]);
+}
+
+exit;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/': {
-        target: 'http://localhost',  // This is your local server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/xamp/htdocs/learningWords/learning-words/php/endpoints'),
-      },
+    '/api': {
+      target: 'http://localhost',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '/learningWords/learningWords/php/endpoints'),
     },
+  },
   },
 });
